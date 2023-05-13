8211afbc10de1d6c5356f29021b9e13b3c9df450
Taekeun Hwang <tkhwang@gmail.com>
2023-05-13 16:00:05 +0900

    feat(chat): :sparkles: add login functionality

--- chat/chat-using-gcp/src/components/UnauthenticatedApp/index.jsx
+++ chat/chat-using-gcp/src/components/UnauthenticatedApp/index.jsx
@@ -1,5 +1,17 @@
+import {useAuth} from '../../hooks/useAuth';
+import './styles.css';
+
 const UnauthenticatedApp = () => {
-  return <div>I'm unauthenticated !!</div>;
+  const {login} = useAuth();
+
+  return (
+    <>
+      <h2>Log in to join a chat room!</h2>
+      <div>
+        <button onClick={login}>Log with Google</button>
+      </div>
+    </>
+  );
 };
 
 export {UnauthenticatedApp};
