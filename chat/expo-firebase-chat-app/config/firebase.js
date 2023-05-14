7c25b64546b1e3e4c3a1d37f205e97c6d140d7cb
Taekeun Hwang <tkhwang@gmail.com>
2023-05-14 16:04:07 +0900

    feat(chat): :sparkles: setup basic screen

--- chat/expo-firebase-chat-app/config/firebase.js
+++ chat/expo-firebase-chat-app/config/firebase.js
@@ -2,6 +2,8 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import Constants from "expo-constants";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,6 +20,6 @@ const firebaseConfig = {
   measurementId: Constants.manifest.extra.measurementId,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+export const database = getFirestore();
