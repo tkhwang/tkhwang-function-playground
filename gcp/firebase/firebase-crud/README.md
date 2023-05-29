e7f18e5229ae9406606f639ef0c4ef349275a334
tkhwang <tkhwang@gmail.com>
2023-05-29 14:47:12 +0900

    ✨ feat(firebase): implement basic crud using firestore

--- gcp/firebase/firebase-crud/README.md
+++ gcp/firebase/firebase-crud/README.md
@@ -1,3 +1,30 @@
 # CRUD Tutorial Using React + Firebase | Firebase 9 and Firestore Tutorial
 
 [CRUD Tutorial Using React + Firebase | Firebase 9 and Firestore Tutorial](https://www.youtube.com/watch?v=jCY6DH8F4oc)
+
+## Config
+
+```javascript
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+```
+
+## Reference
+
+```javascript
+const usersCollectionRef = collection(db, "users");
+```
+
+## Read
+
+```javascript
+const data = await getDocs(usersCollectionRef);
+```
+
+## Write
+
+```javascript
+await addDoc(usersCollectionRef, { name, age });
+```
--- gcp/firebase/firebase-crud/src/App.js
+++ gcp/firebase/firebase-crud/src/App.js
@@ -1,7 +1,67 @@
+import { useEffect, useState } from "react";
+import { db } from "./firebase";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  updateDoc,
+  doc,
+} from "firebase/firestore";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [users, setUsers] = useState([]);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+
+  const usersCollectionRef = collection(db, "users");
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getUsers();
+  }, []);
+
+  const createUser = async () => {
+    await addDoc(usersCollectionRef, { name, age: Number(age) });
+  };
+
+  const updateUser = async (id, age) => {
+    const userDoc = doc(db, "users", id);
+
+    const newFields = { age: age + 1 };
+    await updateDoc(userDoc, newFields);
+  };
+
+  return (
+    <div className="App">
+      <input
+        placeholder="Name..."
+        onChange={(event) => setName(event.target.value)}
+      />
+      <input
+        placeholder="Age..."
+        onChange={(event) => setAge(event.target.value)}
+      />
+
+      <button onClick={createUser}>Create User</button>
+      {users.map((user) => {
+        return (
+          <>
+            <h1>Name: {user.name}</h1>
+            <h1>Age: {user.age}</h1>
+            <button onClick={() => updateUser(user.id, user.age)}>
+              Increase age
+            </button>
+          </>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
