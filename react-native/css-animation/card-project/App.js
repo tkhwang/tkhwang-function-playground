e18f368739985684b9c2c8a5aa777ad0d7c65d2a
tkhwang <tkhwang@gmail.com>
2023-09-30 15:07:22 +0900

    🌈 style(react-native): apply animation

--- react-native/css-animation/card-project/App.js
+++ react-native/css-animation/card-project/App.js
@@ -3,16 +3,6 @@ import { Animated, PanResponder, View } from "react-native";
 import styled from "styled-components/native";
 import { Ionicons } from "@expo/vector-icons";
 
-export default function App() {
-  return (
-    <Container>
-      <Card>
-        <Ionicons name="pizza" color="#000" size={98} />
-      </Card>
-    </Container>
-  );
-}
-
 const Container = styled.View`
   flex: 1;
   justify-content: center;
@@ -29,3 +19,44 @@ const Card = styled(Animated.createAnimatedComponent(View))`
   border-radius: 12px;
   box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.2);
 `;
+
+export default function App() {
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (_, { dx }) => {
+        position.setValue(dx);
+      },
+      onPanResponderGrant: () => onPressIn(),
+      onPanResponderRelease: () => {
+        Animated.parallel([
+          onPressOut,
+          Animated.spring(position, {
+            toValue: 0,
+            useNativeDriver: true,
+          }),
+        ]).start();
+      },
+    })
+  ).current;
+  const scale = useRef(new Animated.Value(1)).current;
+  const position = useRef(new Animated.Value(0)).current;
+  const onPressIn = () =>
+    Animated.spring(scale, { toValue: 0.95, useNativeDriver: true }).start();
+  const onPressOut = Animated.spring(scale, {
+    toValue: 1,
+    useNativeDriver: true,
+  });
+  return (
+    <Container>
+      <Card
+        {...panResponder.panHandlers}
+        style={{
+          transform: [{ scale }, { translateX: position }],
+        }}
+      >
+        <Ionicons name="pizza" color="#192a56" size={98} />
+      </Card>
+    </Container>
+  );
+}
