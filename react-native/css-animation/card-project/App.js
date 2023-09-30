b9bc1bf3d261c85cb6a202642f515c8ed14935dc
tkhwang <tkhwang@gmail.com>
2023-09-30 14:53:57 +0900

    ✨ feat(react-native): setup App.js

--- react-native/css-animation/card-project/App.js
+++ react-native/css-animation/card-project/App.js
@@ -1,8 +1,7 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { Animated, StyleSheet, Text, View } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import React, { useRef } from "react";
+import { Animated, PanResponder, View } from "react-native";
 import styled from "styled-components/native";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function App() {
   return (
@@ -21,10 +20,12 @@ const Container = styled.View`
   background-color: #00a8ff;
 `;
 
-const Card = styled.View`
+const Card = styled(Animated.createAnimatedComponent(View))`
+  background-color: white;
   width: 300px;
   height: 300px;
   justify-content: center;
   align-items: center;
-  background-color: white;
+  border-radius: 12px;
+  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.2);
 `;
